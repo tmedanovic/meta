c3cd959089d911a8b9eb8e40283796361f1d8274
Burak Tasci <me@fulls1z3.com>
2016-11-09 18:19:43 +0200

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { PageTitlePositioning } from './models/page-title-positioning';
+export { MetadataSettings } from './models/metadata-settings';
+export { MetadataService } from './metadata.service';
+export { MetadataModule } from './metadata.module';
